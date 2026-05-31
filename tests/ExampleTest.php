33132a0596c085e35846e6c096573266308fc9e5
OgBek <bbekam60@gmail.com>
2026-05-31 17:41:58 +0300

    test bug fixed

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bekambeyene\Telebirr\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_it_can_be_instantiated()
+    {
+        $this->assertTrue(true);
+    }
+}
